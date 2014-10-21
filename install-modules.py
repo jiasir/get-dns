48facd02c7b20444f9627be0ebe090524e553e27
jiasir <jiasir@icloud.com>
2014-10-21 21:01:08 +0800

    Add install modules features

--- install-modules.py
+++ install-modules.py
@@ -1,6 +1,6 @@
 __author__ = 'Taio'
 
-
+import os
 from utils.noflib import Noflib
 import logging
 
@@ -12,6 +12,7 @@ run = Noflib()
 
 
 
+
 def get_modules():
     '''get modules from github'''
     run.execute_get_output('git', 'clone', 'https://github.com/rthalley/dnspython.git')
@@ -20,3 +21,21 @@ def install_modules():
     '''install dnspython to package-sites'''
     run.execute_get_output('python', 'setup.py', 'install')
 
+
+def main():
+    if not os.path.exists('/var/log/get-dns'):
+       run.execute_get_output('sudo', 'mkdir', '/var/log/get-dns')
+
+    logger.info('Getting modules')
+    get_modules()
+    logger.info('Installing modules')
+    install_modules()
+
+
+
+if __name__ == '__main__':
+    if os.getuid() == 0:
+        main()
+    else:
+        print 'You do not have permission, please run as root.'
+        exit()
